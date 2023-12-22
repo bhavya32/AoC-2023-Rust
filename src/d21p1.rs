f79988fcdd7a96f42b7208166dac05929d63b7ac
bhavya32 <55047586+bhavya32@users.noreply.github.com>
2023-12-22 16:45:13 +0530

    day 21, p2 wrong

--- src/d21p1.rs
+++ src/d21p1.rs
@@ -1,48 +1,46 @@
 #![allow(non_snake_case)]
 #![allow(dead_code)]
-use std::collections::VecDeque;
+#![allow(unused_imports)]
+use std::collections::{VecDeque, HashSet};
+use std::time::Instant;
 use std::{fs, collections::HashMap};
+
 use num::Integer;
+//use std::time::Instant;
+
+fn pr(matrix: & Vec<Vec<char>>) {
+    for x in matrix {
+        println!("{}", x.into_iter().collect::<String>());
+    }
+}
+
 
 fn bfs(m:Vec<Vec<char>>, (si, sj):(usize, usize)) {
     let maxrow = m.len() as i32 - 1;
     let maxcol = m[0].len() as i32 -1;
+    let max_dist = 64;
     let nb = [(-1, 0), (0, -1), (0, 1), (1, 0)];
     let mut queue:VecDeque<(i32,i32, usize)> = VecDeque::new();
     let mut distmap:HashMap<(i32, i32), usize> = HashMap::new();
-
     queue.push_back((si as i32, sj as i32, 0));
-    distmap.insert((si as i32, sj as i32), 0);
-
     while let Some((i, j, dist)) = queue.pop_front() {
+        if dist == max_dist {break;}
         for (dx, dy) in nb {
             let (ni, nj) = (i + dx, j + dy);
             if ni < 0 || nj < 0 || ni > maxrow || nj > maxcol || m[ni as usize][nj as usize] == '#' {continue;}
-            
             if !distmap.contains_key(&(ni, nj)) {
                 distmap.insert((ni, nj), dist + 1);
                 queue.push_back((ni, nj, dist + 1));
             }
         }
     }
-
-    println!("{}", distmap.values().filter(|d| **d <= 64 && d.is_even()).count());
-    let even_corners = distmap.values().filter(|v| **v % 2 == 0 && **v > 65).count();
-    let odd_corners = distmap.values().filter(|v| **v % 2 == 1 && **v > 65).count();
-    let even_full = distmap.values().filter(|v| **v % 2 == 0).count();
-    let odd_full = distmap.values().filter(|v| **v % 2 == 1).count();
-
-    // This is 202300 but im writing it out here to show the process
-    let n = 202300;
-
-    let p2 = ((n+1)*(n*1)) * odd_full + (n*n) * even_full - (n+1) * odd_corners + n * even_corners;
-    println!("{}", p2);
-    //ans is 612941134797232
-    println!("should be - 612941134797232");
+    println!("{}", distmap.values().filter(|d| d.is_even()).count());
+    
+    //println!("{}", queue.len() + 1);
 }
 
 fn main() {
-    let text = fs::read_to_string("./src/d21input.txt").expect("Unable to read file");
+    let text = fs::read_to_string("./q.txt").expect("Unable to read file");
     let lines = text.split("\r\n");
     let mut matrix:Vec<Vec<char>> = Vec::new();
     let (mut si, mut sj) = (0, 0);
@@ -59,5 +57,13 @@ fn main() {
         matrix.push(t);
     }
 
+    //bfs(matrix, (si, sj));
+    //println!("{:?}", djinplen);
+    
+    let now = Instant::now();
+    //let res = d20(modules, djinplen);
     bfs(matrix, (si, sj));
+    let elapsed = now.elapsed();
+    println!("Elapsed: {:.2?}", elapsed);
+    //println!("{}", res);
 }
